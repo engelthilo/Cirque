1b8feacb44b91c9b9362e5420f84faa37fffd4e7
Frederik <fhov@itu.dk>
2014-12-05 12:52:34 +0100

    hbox fix

--- src/model/DBConnect.java
+++ src/model/DBConnect.java
@@ -177,13 +177,13 @@ public class DBConnect {
      */
     public LinkedHashMap<Integer, HBox> getReservations(String phoneNumber) {
         LinkedHashMap<Integer, HBox> reservations = new LinkedHashMap<Integer, HBox>();
+        HBox hbox = new HBox(50);
         try {
             st = getCon().createStatement();
             String query = "SELECT shows.id, time, movie_name, cinema_name, reservations.id, customer_name FROM reservations, cinemas, movies, shows WHERE reservations.customer_phone='" + phoneNumber + "' AND reservations.show_id = shows.id AND shows.movie_id = movies.id AND shows.cinema_id = cinemas.id";
             rs = st.executeQuery(query);
             while(rs.next()) {
                 int id = rs.getInt("reservations.id");
-                HBox hbox = new HBox(50);
                 Label movieName = new Label(rs.getString("movie_name"));
                 Label cinemaName = new Label(rs.getString("cinema_name"));
                 Label customerName = new Label(rs.getString("customer_name"));
