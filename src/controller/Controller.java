567cb34de2d2776d7a93f931b9f0822c787e822f
Mark Mortensen <rostgaard@Marks-MacBook-Pro.local>
2014-12-09 12:59:32 +0100

    Added rapport

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -692,8 +692,8 @@ public class Controller {
         dialog.show();
     }
 
-    public javafx.scene.control.TextField getPhoneNumber() {
-        return phoneNumber;
-    }
+    //public javafx.scene.control.TextField getPhoneNumber() {
+        //return phoneNumber;
+    //}
 
 }
