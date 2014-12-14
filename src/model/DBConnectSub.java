dce9ec8895aad54318b17bfca00b27105458769d
Mark Mortensen <rostgaard@Marks-MacBook-Pro.local>
2014-12-14 17:33:15 +0100

    changed DBConnectSUB

--- src/model/DBConnectSub.java
+++ src/model/DBConnectSub.java
@@ -1,10 +1,26 @@
 package model;
 
 
-public class DBConnectSub extends DBConnectSub{
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-    public DBConnect() {
+public class DBConnectSub extends DBConnect{
+    private Connection con;
+    private Statement st;
+    private ResultSet rs;
 
+
+    public DBConnectSub() {
+        //Så skal du bare skrive SQL databse site (altså ''mysql://mysql.itu.dk:3306/kaffeklubben'' brugernavn (kaffeklubben) og kodeord (kp8473moxa
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://mysql.itu.dk:3306/kaffeklubben", "kaffeklubben", "kp8473moxa");
+            st = con.createStatement();
+        } catch (Exception e) {
+            System.out.println("Error:" + e);
+        }
     }
-
+    
 }
