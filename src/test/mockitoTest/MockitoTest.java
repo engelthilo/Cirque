378f2cf32f800ae9a9de348881ecd931fa1e3bed
Amanda Engel Thilo <engel.thilo@gmail.com>
2014-12-16 10:39:24 +0100

    mockito

---
+++ src/test/mockitoTest/MockitoTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.when;
+
+public class MockitoTest {
+
+
+
+}
